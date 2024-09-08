3442a1b34f2fee0c2f59cd626e37ebcb8af5a6e0
Ahmed Abdelaal <ahmedabdelaal4563@gmail.com>
2024-09-08 15:04:32 +0300

    feat: prometheus client setup
    - django-prometheus client setup done

--- PrometheusIntegration/PrometheusIntegration/settings.py
+++ PrometheusIntegration/PrometheusIntegration/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_api',
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware', 
 ]
 
 ROOT_URLCONF = 'PrometheusIntegration.urls'
--- PrometheusIntegration/hello_api/views.py
+++ PrometheusIntegration/hello_api/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
 # hello_api/views.py
+from prometheus_client import Counter
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+# Define a counter metric to count requests
+REQUEST_COUNTER = Counter('hello_world_requests', 'Number of requests to the hello world endpoint')
 
 @csrf_exempt  # Disable CSRF protection for simplicity in this example
 def hello_world(request):
